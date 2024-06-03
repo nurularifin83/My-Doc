ab6d9176d13cb9408a3692526a0bb43bc6efe024
nurularifin83 <nurularifin837@gmail.com>
2024-06-03 10:15:57 +0700

    Tutorial how to add TinyMCE self Hosted

---
+++ lms/add-data-with-fetch-javascript.blade.php
@@ -0,0 +1,101 @@
+<!-- ROUTE -->
+@php
+Route::controller(CourseController::class)->group(function(){
+    Route::post('/save-lecture/','SaveLecture')->name('save-lecture');
+});    
+@endphp
+
+<!--FORM-->
+<div class="container">
+    <h6>Lecture Title </h6>
+    <input type="text" class="form-control" placeholder="Enter Lecture Title">
+    <textarea class="form-control mt-2" placeholder="Enter Lecture Content" rows="10" id="smalleditor"></textarea>
+    <h6 class="mt-3">Add Video Url</h6>
+    <input type="text" name="url" class="form-control" placeholder="Add URL">
+
+    <button class="btn btn-primary mt-3" onclick="saveLecture('${courseId}',${sectionId},'${containerId}')" >Save Lecture</button>
+    <button class="btn btn-secondary mt-3" onclick="hideLectureContainer('${containerId}')">Cancel</button>
+</div>
+<!--FORM-->
+
+<!-- JAVASCRIPT-->
+<script>
+    const saveLecture = (courseId, sectionId, containerId) => {
+        const lectureContainer = document.getElementById(containerId);
+        const lectureTitle = lectureContainer.querySelector('input[type="text"]').value;
+        const lectureContent = lectureContainer.querySelector('textarea').value;
+        const lectureUrl = lectureContainer.querySelector('input[name="url"]').value;
+
+        // Request option
+        const requestOptions = {
+            method: 'POST',
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': '{{ csrf_token() }}',
+            },
+            body: JSON.stringify({
+                course_id: courseId,
+                section_id: sectionId,
+                lecture_title: lectureTitle,
+                lecture_url: lectureUrl,
+                content: lectureContent,
+            }),
+        };
+        
+        fetch('/save-lecture', requestOptions)
+        .then(response => {
+            if(!response.ok){
+                throw new Error('Network response was not ok.');
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+
+            lectureContainer.style.display = 'none';
+            location.reload();
+            
+            // Start Message 
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                icon: 'success', 
+                showConfirmButton: false,
+                timer: 6000 
+            })
+            if ($.isEmptyObject(data.error)) {  
+                Toast.fire({
+                type: 'success',
+                title: data.success, 
+                })
+            }else{
+                Toast.fire({
+                    type: 'error',
+                    title: data.error, 
+                    })
+                }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation: '+error);
+        });
+    }
+</script>
+
+<!-- FUNCTION -->
+@php
+    public function SaveLecture(Request $request)
+    {
+        $lecture = new CourseLecture();
+        $lecture->course_id = $request->course_id;
+        $lecture->section_id = $request->section_id;
+        $lecture->lecture_title = $request->lecture_title;
+        $lecture->url = $request->lecture_url;
+        $lecture->content = $request->content;
+        $lecture->save();
+
+        return response()->json(['success' => 'Saved.']);
+    }
+@endphp
---
+++ lms/install-TinyMCE-selfhosted.blade.php
@@ -0,0 +1,100 @@
+<!--1. Install tinymce visit this link: https://www.tiny.cloud/docs/tinymce/latest/npm-projects/  or just type: npm install tinymce-->
+<!--2. Import TinyMCE Files
+    In the resources/js/app.js file, add the following lines at the top to import the required JS and CSS files for TinyMCE: resources/js/app.js
+
+    import './bootstrap';
+    import 'tinymce/tinymce';
+    import 'tinymce/skins/ui/oxide/skin.min.css';
+    import 'tinymce/skins/content/default/content.min.css';
+    import 'tinymce/skins/content/default/content.css';
+    import 'tinymce/icons/default/icons';
+    import 'tinymce/themes/silver/theme';
+    import 'tinymce/models/dom/model';
+
+-->
+
+<!--3. Add TinyMCE Init Code
+Now add the following init code to the bottom of your app.js
+
+// .. After imports init TinyMCE ..
+window.addEventListener('DOMContentLoaded', () => {
+    tinymce.init({
+        selector: 'textarea',
+
+        /* TinyMCE configuration options */
+        skin: false,
+        content_css: false
+    });
+});
+
+-->
+
+<!--4. Build Your Assets Using Vite
+Build the assets of your Laravel application using Vite by running the following command in your terminal:
+
+npm run build
+
+-->
+
+<!--5. Add TinyMCE to Blade by Using the @vite Directive
+Now, in your Blade file, add the following line to use Vite for including the app.js file that was built using npm: @vite(['resources/js/app.js']).
+
+Xample: -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laracoding.com TinyMCE Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <h2>Laracoding.com - TinyMCE Example</h2>
+    <form action="" method="get">
+        <div class="mb-3">
+            <label for="pwd">TinyMCE input:</label>
+            <textarea class="tinyMce" name="user-bio"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+</html>
+
+<!-- Code in app.js after completed -->
+<script>
+    
+import './bootstrap';
+// Tinymce
+import 'tinymce/tinymce';
+import 'tinymce/skins/ui/oxide/skin.min.css';
+import 'tinymce/skins/content/default/content.min.css';
+import 'tinymce/skins/content/default/content.css';
+import 'tinymce/icons/default/icons';
+import 'tinymce/themes/silver/theme';
+import 'tinymce/models/dom/model';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+// TinyMCE
+window.addEventListener('DOMContentLoaded', () => {
+    tinymce.init({
+        selector: 'textarea#editor',
+        height: 680,
+        skin: false,
+        content_css: false
+    });
+
+    tinymce.init({
+        selector: 'textarea#smalleditor',
+        height: 350,
+        skin: false,
+        content_css: false
+    });
+});
+
+</script>
